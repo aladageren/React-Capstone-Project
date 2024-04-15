8dd0b9fee3e677af990181b48b32b84be4765cdb
Eren Aladağ <aladageren@gmail.com>
2024-04-15 21:36:06 +0300

    responsive css layout

--- src/semantic_jsx/Footer.js
+++ src/semantic_jsx/Footer.js
@@ -2,31 +2,38 @@ function Footer() {
     return (
         <>
             <footer>
-                <img src="/Logo.svg" alt="Logo"></img>
+                <article>
+                    <img src="/Logo.svg" alt="Logo"></img>
+                </article>
+                <article>
+                    <h4>Doormat Navigation</h4>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/menu">Menu</a></li>
+                        <li><a href="/reservations">Reservations</a></li>
+                        <li><a href="/order">Order Online</a></li>
+                        <li><a href="/login">Login</a></li>
+                    </ul>
+                </article>
 
-                <h4>Doormat Navigation</h4>
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/menu">Menu</a></li>
-                    <li><a href="/reservations">Reservations</a></li>
-                    <li><a href="/order">Order Online</a></li>
-                    <li><a href="/login">Login</a></li>
-                </ul>
+                <article>
+                    <h4>Contact</h4>
+                    <ul>
+                        <li><a href="/">Address</a></li>
+                        <li><a href="/">Phone Number</a></li>
+                        <li><a href="/">Email</a></li>
+                    </ul>
+                </article>
 
-                <h4>Contact</h4>
-                <ul>
-                    <li><a href="/">Address</a></li>
-                    <li><a href="/">Phone Number</a></li>
-                    <li><a href="/">Email</a></li>
-                </ul>
-
-                <h4>Social Media Links</h4>
-                <ul>
-                    <li><a href="/">Facebook</a></li>
-                    <li><a href="/">Instagram</a></li>
-                    <li><a href="/">Twitter</a></li>
-                </ul>
+                <article>
+                    <h4>Social Media Links</h4>
+                    <ul>
+                        <li><a href="/">Facebook</a></li>
+                        <li><a href="/">Instagram</a></li>
+                        <li><a href="/">Twitter</a></li>
+                    </ul>
+                </article>
             </footer>
         </>
     );
